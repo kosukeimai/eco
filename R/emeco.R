a348fd345c2b73d7ffa55dbf7fa7e5fbf501d7e8
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2004-09-11 02:40:55 +0000

    initial try

---
+++ R/emeco.R
@@ -0,0 +1,78 @@
+
+eco.em <- function(Y, X, data = parent.frame(), 
+		n.draws = 300, supplement=NULL, theta.t){ 
+
+  ## checking inputs
+  if ((dim(supplement)[2] != 2) && (length(supplement)>0))
+    stop("Error: use n by 2 matrix for survey data")
+  
+  call <- match.call()
+
+  ff <- as.formula(paste(call$Y, "~ -1 +", call$X))
+  if (is.matrix(eval.parent(call$data)))
+    data <- as.data.frame(data)
+  X <- model.matrix(ff, data)
+  Y <- model.response(model.frame(ff, data=data))
+
+  ##survey data
+  if (length(supplement) == 0) {
+    survey.samp <- 0
+    survey.data <- 0
+    survey.yes<-0
+  }
+  else {
+    survey.samp <- length(supplement[,1])
+    survey.data <- as.matrix(supplement)
+    survey.yes<-1
+  }
+  
+  ind<-c(1:length(X))
+  X1type<-0
+  X0type<-0
+  samp.X1<-0
+  samp.X0<-0
+  X1.W1<-0
+  X0.W2<-0
+  
+  ##Xtype x=1
+  X1.ind<-ind[along=(X==1)]
+  if (length(X[X!=1])<length(X)){
+    X1type<-1
+    samp.X1<-length(X1.ind)
+    X1.W1<-Y[X1.ind]
+  }
+  
+  ##Xtype x=0
+  X0.ind<-ind[along=(X==0)]
+  if (length(X[X!=0])<length(X)){
+    X0type<-1
+    samp.X0<-length(X0.ind)
+    X0.W2<-Y[X0.ind]
+  }
+  
+  XX.ind<-setdiff(ind, union(X0.ind, X1.ind))
+  X.use<-X[XX.ind]
+  Y.use<-Y[XX.ind]
+
+  order.old<-order(c(XX.ind, X0.ind, X1.ind))
+  
+  ## fitting the model
+  n.samp <- length(Y.use)	 
+  d <- cbind(X.use, Y.use)
+
+    res <- .C("cBaseeco", as.double(d), as.double(theta.t),
+	      as.integer(n.samp),
+	      as.integer(n.draws), 
+              as.integer(survey.yes), as.integer(survey.samp), as.double(survey.data),
+   	      as.integer(X1type), as.integer(samp.X1), as.double(X1.W1),
+   	      as.integer(X0type), as.integer(samp.X0), as.double(X0.W2),
+	      pdTheta=double(5)
+              PACKAGE="eco")
+    red.out < -pdTheta     
+    
+  }
+  class(res.out) <- "eco"
+  return(res.out)
+}
+
+
