3115c8e6b253f74ed0732af1f3c24b3c75e14ee2
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2005-06-15 21:38:44 +0000

    change back to x from object

--- R/print.summary.eco.R
+++ R/print.summary.eco.R
@@ -1,25 +1,25 @@
-print.summary.eco <- function(object, digits=max(3, getOption("digits")
+print.summary.eco <- function(x, digits=max(3, getOption("digits")
                                    -3), ...) {
   cat("\nCall: ") 
-  cat(paste(deparse(object$call), sep="\n", collapse="\n"))
+  cat(paste(deparse(x$call), sep="\n", collapse="\n"))
 
-  if (!is.null(object$nonpar))
-    if(object$nonpar)
+  if (!is.null(x$nonpar))
+    if(x$nonpar)
       cat("\n\n** Nonparametric Model **")
     else
       cat("\n\n** Parametric Model **")
   
   cat("\n\nAggregate Estimates:\n")
-  printCoefmat(object$agg.table, digits=digits, na.print="NA",...)
+  printCoefmat(x$agg.table, digits=digits, na.print="NA",...)
 
-  cat("\nNumber of Units:", object$n.obs)
+  cat("\nNumber of Units:", x$n.obs)
   
-  if (!is.null(object$W1.table)) {
+  if (!is.null(x$W1.table)) {
     cat("\n\nUnit-level Estimates of W1:\n")
-    printCoefmat(object$W1.table, digits=digits, na.print="NA",...)
+    printCoefmat(x$W1.table, digits=digits, na.print="NA",...)
     cat("\n\nUnit-level Estimates of W2:\n")
-    printCoefmat(object$W2.table, digits=digits, na.print="NA",...)
+    printCoefmat(x$W2.table, digits=digits, na.print="NA",...)
   }
   cat("\n")
-  invisible(object)
+  invisible(x)
 }
