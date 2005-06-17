d3664c75b219187515ce35449aeaf69d6620c7f3
kosukeimai <kimai@princeton.edu>
2005-06-17 18:02:03 +0000

    up

--- R/print.summary.eco.R
+++ R/print.summary.eco.R
@@ -1,12 +1,18 @@
 print.summary.eco <- function(x, digits=max(3, getOption("digits")
                                    -3), ...) {
   cat("\nCall: ") 
-  cat(paste(deparse(x$call), sep="\n", collapse="\n"))
+  cat(paste(deparse(x$call), "\n", sep="\n", collapse="\n"))
 
-  cat("\n\nAggregate Estimates:\n")
+  if (!is.null(x$param.table)) {
+    cat("\nParameter Estimates:\n")
+    printCoefmat(x$param.table, digits=digits, na.print="NA",...)
+  }
+
+  cat("\nAggregate Estimates:\n")
   printCoefmat(x$agg.table, digits=digits, na.print="NA",...)
 
   cat("\nNumber of Units:", x$n.obs)
+  cat("\nNumber of Monte Carlo Draws:", x$n.draws)
   
   if (!is.null(x$W1.table)) {
     cat("\n\nUnit-level Estimates of W1:\n")
@@ -14,6 +20,7 @@ print.summary.eco <- function(x, digits=max(3, getOption("digits")
     cat("\n\nUnit-level Estimates of W2:\n")
     printCoefmat(x$W2.table, digits=digits, na.print="NA",...)
   }
+
   cat("\n")
   invisible(x)
 }
--- R/summary.eco.R
+++ R/summary.eco.R
@@ -1,8 +1,36 @@
 summary.eco <- function(object, CI=c(2.5, 97.5), long = FALSE, ...) {
   
-  n.obs<-ncol(object$W1)
+  n.obs <- ncol(object$W1)
+  n.draws <- nrow(object$W1)
+  
   table.names<-c("mean", "std.dev", paste(min(CI), "%", sep=" "),
 			paste(max(CI), "%", sep=" "))
+
+  if (is.null(object$N))
+    N <- rep(1, nrow(object$X))
+  W1.agg.mean <- object$W1 %*% (object$X*N/sum(object$X*N))
+  W2.agg.mean <- object$W2 %*% ((1-object$X)*N/sum((1-object$X)*N))
+  agg.table <- rbind(cbind(mean(W1.agg.mean), sd(W1.agg.mean), 
+                           quantile(W1.agg.mean, min(CI)/100), 
+                           quantile(W1.agg.mean, max(CI)/100)),
+                     cbind(mean(W2.agg.mean), sd(W2.agg.mean), 
+                           quantile(W2.agg.mean, min(CI)/100), 
+                           quantile(W2.agg.mean, max(CI)/100)))
+  colnames(agg.table) <- table.names
+  rownames(agg.table) <- c("W1", "W2")
+
+  param <- cbind(object$mu, invlogit(object$mu), object$Sigma)
+  if (is.null(param))
+    param.table <- NULL
+  else {
+    param.table <- cbind(apply(param, 2, mean), apply(param, 2, sd),
+                         apply(param, 2, quantile, min(CI)/100),
+                         apply(param, 2, quantile, max(CI)/100))
+    colnames(param.table) <- table.names
+    rownames(param.table) <- c("mu1", "mu2", "E(W1)", "E(W2)",
+                               "Sigma11", "Sigma12", "Sigma22")
+  }
+  
   if (long) {
     W1.table <- cbind(apply(object$W1, 2, mean), apply(object$W1, 2, sd),
                       apply(object$W1, 2, quantile, min(CI)/100),
@@ -15,26 +43,10 @@ summary.eco <- function(object, CI=c(2.5, 97.5), long = FALSE, ...) {
   }
   else
     W1.table <- W2.table <- NULL
-  W1.agg.mean <- apply(object$W1, 1, mean)
-  W2.agg.mean <- apply(object$W2, 1, mean)
-  agg.table <- rbind(cbind(mean(W1.agg.mean), sd(W1.agg.mean), 
-                           quantile(W1.agg.mean, min(CI)/100), 
-                           quantile(W1.agg.mean, max(CI)/100)),
-                     cbind(mean(W2.agg.mean), sd(W2.agg.mean), 
-                           quantile(W2.agg.mean, min(CI)/100), 
-                           quantile(W2.agg.mean, max(CI)/100)))
-  colnames(agg.table) <- table.names
-  rownames(agg.table) <- c("W1", "W2")
 
-  if (is.null(object$mu))
-    mu.table <- NULL
-  else
-    mu.table <- cbind(apply(object$mu, 2, mean), apply(object$mu, 2, sd),
-                      apply(object$mu, 2, quantile, min(CI)/100),
-                      apply(object$mu, 2, quantile, max(CI)/100))
-    
   ans <- list(call = object$call, W1.table = W1.table, W2.table = W2.table,
-              agg.table = agg.table, mu.table = mu.table, n.obs = n.obs) 
+              agg.table = agg.table, param.table = param.table,
+              n.draws = n.draws, n.obs = n.obs) 
   class(ans) <-"summary.eco"
   return(ans)
 }
