a812f6dd5d5be5cae46a62a584ee35a5193d7277
kosukeimai <kimai@princeton.edu>
2005-06-17 15:49:08 +0000

    up

---
+++ R/summary.predict.eco.R
@@ -0,0 +1,19 @@
+summary.predict.eco <- function(object, CI=c(2.5, 97.5), ...) {
+  
+  n.draws <- ncol(object)
+  table.names<-c("mean", "std.dev", paste(min(CI), "%", sep=" "),
+			paste(max(CI), "%", sep=" "))
+
+  W.table <- rbind(cbind(mean(object[,1]), sd(object[,1]), 
+                         quantile(object[,1], min(CI)/100), 
+                         quantile(object[,1], max(CI)/100)),
+                   cbind(mean(object[,2]), sd(object[,2]), 
+                         quantile(object[,2], min(CI)/100), 
+                         quantile(object[,2], max(CI)/100)))
+  colnames(W.table) <- table.names
+  rownames(W.table) <- c("W1", "W2")
+
+  res <- list(W.table = W.table, n.draws = n.draws)
+  class(res) <- "summary.predict.eco"
+  return(res)
+}
