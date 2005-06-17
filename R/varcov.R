8ea474126bf9612aec2e38519f3d4835bcb7e25a
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2005-06-17 19:45:00 +0000

    modify varcov.ecoNP code

--- R/varcov.R
+++ R/varcov.R
@@ -3,26 +3,25 @@ varcov <- function(object, ...)
 
 varcov.ecoNP <- function(object, subset = NULL, ...) {
   if (is.null(subset))
-    subset <- 1:nrow(object$Sigma11)
-  else if (max(subset) > nrow(object$Sigma11))
-    stop(paste("invalid input for `subset.' only", nrow(object$Sigma11), "draws are stored."))
+    subset <- 1:nrow(object$Sigma[,,1])
+  else if (max(subset) > nrow(object$Sigma[,,1]))
+    stop(paste("invalid input for `subset.' only", nrow(object$Sigma[,,1]), "draws are stored."))
   
-  sigma11 <- object$Sigma11[subset,]
-  sigma12 <- object$Sigma12[subset,]
-  sigma22 <- object$Sigma22[subset,]
 
-  p <- 2
-  n <- length(sigma11)
+  p <- ncol(Sigma[,,1])
+  n <- length(subset)
+  nobs <- length(cov[1,1,])
   Sigma <- array(0, c(p, p, n))
-  cov <- as.matrix(cbind(as.vector(sigma11), as.vector(sigma12), as.vector(sigma22)))
-  for (i in 1:n) {
-    count <- 1
-    for (j in 1:p) {
-      Sigma[j,j:p,i] <- cov[i,count:(count+p-j)]
-      count <- count + p - j + 1
-    }
-   diag(Sigma[,,i]) <- diag(Sigma[,,i]/2)
-    Sigma[,,i] <- Sigma[,,i] + t(Sigma[,,i])
+  cov <- object$Sigma
+  for (k in 1:nobs)
+    for (i in 1:n) {
+      count <- 1
+      for (j in 1:p) {
+       Sigma[j,j:p,(k-1)*nobs+i] <- cov[subset[i],count:(count+p-j),k]
+       count <- count + p - j + 1
+      }
+      diag(Sigma[,,(k-1)*nobs+i]) <- diag(Sigma[,,(k-1)*nobs+i]/2)
+      Sigma[,,(k-1)*nobs+i] <- Sigma[,,(k-1)*nobs+i] + t(Sigma[,,(k-1)*nobs+i])
   }
   if (n > 1)
     return(Sigma)
