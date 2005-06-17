29fe4953f6ace3d701b00e8fdf06aa70f28c7404
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2005-06-17 18:30:31 +0000

    data checking addition

--- R/coef.eco.R
+++ R/coef.eco.R
@@ -1,6 +1,9 @@
 coef.eco <- function(object, subset = NULL, ...) {
   if (is.null(subset))
-    return(object$mu)
-  else
+    subset <- 1:nrow(object$mu)
+  else if (max(subset) > nrow(object$mu))
+    stop(paste("invalid input for `subset.' only", nrow(object$mu), "draws are stored."))
+
     return(object$mu[subset,])
+
 }
