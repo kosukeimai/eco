b2a6f80edbd27440b1ffaf0648f908c9bd62a567
kosukeimai <kimai@princeton.edu>
2005-06-17 00:43:40 +0000

    change n to n.draws

--- R/predict.eco.R
+++ R/predict.eco.R
@@ -5,11 +5,11 @@ predict.eco <- function(object, newdata = NULL, newdraw = NULL, ...){
   if (is.null(object$Sigma) && is.null(newdraw$Sigma))
     stop("Posterior draws of Sigma must be supplied.")   
   mu <- object$mu
-  n <- nrow(mu)
+  n.draws <- nrow(mu)
   p <- ncol(mu)
   Sigma <- cov.eco(object)
-  Wstar <- matrix(NA, nrow=n, ncol=p)
-  for (i in 1:n) 
+  Wstar <- matrix(NA, nrow=n.draws, ncol=p)
+  for (i in 1:n.draws) 
     Wstar[i,] <- mvrnorm(1, mu = mu[i,], Sigma = Sigma[,,i])
   return(apply(Wstar, 2, invlogit))
 }
