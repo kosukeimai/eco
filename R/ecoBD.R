890921a47bb767c94b06ef291bba888ecb844fa6
kosukeimai <kimai@princeton.edu>
2005-06-24 15:46:29 +0000

    up

--- R/ecoBD.R
+++ R/ecoBD.R
@@ -11,12 +11,14 @@ bounds <- function(formula, data = parent.frame(), N=NULL){
   n.obs <- nrow(X)
   if (sum(apply(X, 1, sum) == 1) != n.obs)
     X <- cbind(X, 1-X)
-  ## for now, assume Y is a vector and X can be a matrix
-  Wmin <- Wmax <- array(NA, c(n.obs, ncol(X), ncol(Y)))
-  for (i in 1:ncol(X))
-    for (j in 1:ncol(Y)) {
+  C <- ncol(X)
+  R <- ncol(Y)
+  Wmin <- Wmax <- array(NA, c(n.obs, C, R))
+  for (i in 1:C)
+    for (j in 1:R) {
       Wmin[,i,j] <- apply(cbind(0, (X[,i]+Y[j]-1)/X[,i]), 1, max)
       Wmax[,i,j] <- apply(cbind(1, Y[,j]/X[,1]), 1, min)
     }
-  return(list(call = match.call(), Wmin=Wmin, Wmax=Wmax))
+  return(list(call = match.call(), Wmin = if (R>1) Wmin else Wmin[,,1],
+              Wmax = if (R>1) Wmax else Wmax[,,1]))
 }
