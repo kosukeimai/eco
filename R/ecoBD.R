fbe707adf96d61bd8fbeb42d2f0eccbb028ae550
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2005-06-24 18:31:19 +0000

    fix bugs

--- R/ecoBD.R
+++ R/ecoBD.R
@@ -5,7 +5,7 @@ bounds <- function(formula, data = parent.frame(), N=NULL){
   if (is.matrix(eval.parent(call$data)))
     data <- as.data.frame(data)
   X <- model.matrix(tt, data)
-  Y <- model.response(model.frame(tt, data = data))
+  Y <- as.matrix(model.response(model.frame(tt, data = data)))
   N <- eval(call$N, data)
 
   n.obs <- nrow(X)
@@ -16,8 +16,8 @@ bounds <- function(formula, data = parent.frame(), N=NULL){
   Wmin <- Wmax <- array(NA, c(n.obs, C, R))
   for (i in 1:C)
     for (j in 1:R) {
-      Wmin[,i,j] <- apply(cbind(0, (X[,i]+Y[j]-1)/X[,i]), 1, max)
-      Wmax[,i,j] <- apply(cbind(1, Y[,j]/X[,1]), 1, min)
+      Wmin[,i,j] <- apply(cbind(0, (X[,i]+Y[,j]-1)/X[,i]), 1, max)
+      Wmax[,i,j] <- apply(cbind(1, Y[,j]/X[,i]), 1, min)
     }
   return(list(call = match.call(), Wmin = if (R>1) Wmin else Wmin[,,1],
               Wmax = if (R>1) Wmax else Wmax[,,1]))
