00b3f6bc4b500645d5284fcc728f86338759f1eb
kosukeimai <kimai@princeton.edu>
2005-06-26 02:05:51 +0000

    up

--- R/print.ecoBD.R
+++ R/print.ecoBD.R
@@ -1,14 +1,25 @@
-print.ecoBD <- function(x, digits = max(3, getOption("digits") -3), ...)
-  {
-    cat("\nCall:\n", deparse(x$call), "\n\n", sep="")
-    cat("Aggregate Lower Bounds:\n")
-    print.default(format(x$aggWmin, digits = digits), print.gap = 2, quote =
+print.ecoBD <- function(x, digits = max(3, getOption("digits") -3),
+                        ...) {
+  cat("\nCall:\n", deparse(x$call), "\n\n", sep="")
+  
+  cat("Aggregate Lower Bounds (Proportions):\n")
+  print.default(format(x$aggWmin, digits = digits), print.gap = 2, quote =
+                FALSE)
+  cat("\nAggregate Upper Bounds (Proportions):\n")
+  print.default(format(x$aggWmax, digits = digits), print.gap = 2, quote =
+                FALSE)
+  
+  if (!is.null(x$aggNmin)) {
+    cat("\nAggregate Lower Bounds (Counts):\n")
+    print.default(format(x$aggNmin, digits = digits), print.gap = 2, quote =
                   FALSE)
-    cat("\nAggregate Upper Bounds:\n")
-    print.default(format(x$aggWmax, digits = digits), print.gap = 2, quote =
-                  FALSE)
-    if (!is.null(x$N))
-      cat("\nThe size of aggregate table:", sum(x$N))
-    cat("\n\n")
-    invisible(x)
   }
+  if (!is.null(x$aggNmax)) {
+    cat("\nAggregate Upper Bounds (Counts):\n")
+    print.default(format(x$aggNmax, digits = digits), print.gap = 2, quote =
+                  FALSE)
+  }
+  
+  cat("\n")
+  invisible(x)
+}
