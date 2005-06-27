5776b709528ddce49fc0f7cff9069cb92a8384e4
kosukeimai <kimai@princeton.edu>
2005-06-27 14:09:38 +0000

    up

--- R/ecoRC.R
+++ R/ecoRC.R
@@ -1,7 +1,7 @@
 ecoRC <- function(formula, data = parent.frame(),
-                  mu0 = 0, tau0 = 2, nu0 = 4, S0 = 10, reject = TRUE,
-                  parameter = TRUE, n.draws = 5000,
-                  burnin = 0, thin = 0, verbose = FALSE){ 
+                  mu0 = 0, tau0 = 2, nu0 = 4, S0 = 10, mu.start = 0,
+                  Sigma.start = 1, reject = TRUE, parameter = TRUE,
+                  n.draws = 5000, burnin = 0, thin = 0, verbose = FALSE){ 
   
   ## checking inputs
   if (burnin >= n.draws)
@@ -23,16 +23,32 @@ ecoRC <- function(formula, data = parent.frame(),
   ## fitting the model
   n.store <- floor((n.draws-burnin)/(thin+1))
   n.par <- R
-  tmp <- ecBD(formula, data=data)
+  tmp <- ecoBD(formula, data=data)
   S0 <- diag(S0, C)
   mu0 <- rep(mu0, C)
+  Sigma.start <- diag(Sigma.start, C)
 
   res.out <- list(call = call, X = X, Y = Y, Wmin = tmp$Wmin, Wmax = tmp$Wmax)
   if (R == 1) {
     res <- .C("cBase2C", as.double(X), as.double(Y),
               as.double(tmp$Wmin[,1,]), as.double(tmp$Wmax[,1,]),
-              as.integer(n.samp), as.integer(C),
-              as.integer(reject),
+              as.integer(n.samp), as.integer(C), as.integer(reject),
+              as.integer(n.draws), as.integer(burnin),
+              as.integer(thin+1), as.integer(verbose),
+              as.integer(nu0), as.double(tau0),
+              as.double(mu0), as.double(S0), as.double(mu.start),
+              as.double(Sigma.start),
+              as.integer(parameter), pdSmu = double(n.store*C),
+              pdSSigma = double(n.store*C*(C+1)/2),
+              pdSW = double(n.store*n.samp*C), PACKAGE="eco")
+    res.out$mu <- matrix(res$pdSmu, n.store, C, byrow=TRUE)
+    res.out$Sigma <- matrix(res$pdSSigma, n.store, C*(C+1)/2, byrow=TRUE)
+    res.out$W <- array(res$pdSW, c(C, n.samp, n.store))
+  }
+  else {
+    res <- .C("cBaseRC", as.double(X), as.double(Y),
+              as.double(tmp$Wmin[,1:R,]), as.double(tmp$Wmax[,1:R,]),
+              as.integer(n.samp), as.integer(C), as.integer(reject),
               as.integer(n.draws), as.integer(burnin),
               as.integer(thin+1), as.integer(verbose),
               as.integer(nu0), as.double(tau0),
@@ -40,9 +56,6 @@ ecoRC <- function(formula, data = parent.frame(),
               as.integer(parameter), pdSmu = double(n.store*C),
               pdSSigma = double(n.store*C*(C+1)/2),
               pdSW = double(n.store*n.samp*C), PACKAGE="eco")
-    res.out$mu <- matrix(res$pdSmu, n.store, C, byrow=TRUE)
-    res.out$Sigma <- matrix(res$pdSSigma, n.store, C*(C+1)/2, byrow=TRUE)
-    res.out$W <- array(res$pdSW, c(C, n.samp, n.store))
   }
   
   class(res.out) <- c("ecoRC", "eco")
