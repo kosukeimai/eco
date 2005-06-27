69688dab8afe3ae152b4521677d66bea8795588b
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2005-06-27 20:38:00 +0000

    update

--- R/print.eco.R
+++ R/print.eco.R
@@ -4,8 +4,8 @@ print.eco <- function(x, digits = max(3, getOption("digits") -3),
   
   if (is.null(x$N))
     N <- rep(1, nrow(x$X))
-  W.mean <- cbind(mean(x$W1 %*% (x$X*N/sum(x$X*N))),
-                  mean(x$W2 %*% ((1-x$X)*N/sum((1-x$X)*N))))
+  W.mean <- cbind(mean(x$W[,1,] %*% (x$X*N/sum(x$X*N))),
+                  mean(x$W[,2,] %*% ((1-x$X)*N/sum((1-x$X)*N))))
   colnames(W.mean) <- c("W1", "W2")
   rownames(W.mean) <- "posterior mean"
   
