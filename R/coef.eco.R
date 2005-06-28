458b1cdb5ea5f14f52ccc9ef21ac9707cb185596
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2005-06-28 21:39:54 +0000

    now takes both eco and ecoNP parameter input, for ecoNP, mu=(ndraws*nobs) by 2

--- R/coef.eco.R
+++ R/coef.eco.R
@@ -4,6 +4,15 @@ coef.eco <- function(object, subset = NULL, ...) {
   else if (max(subset) > nrow(object$mu))
     stop(paste("invalid input for `subset.' only", nrow(object$mu), "draws are stored."))
 
-    return(object$mu[subset,])
+    if (length(dim(object$mu))==2) out <- object$mu[subset,]
+	else if (length(dim(object$mu))==3) 
+           {
+	      out <- NULL
+	      nobs <- dim(object$mu)[3]
+	      for ( i in nobs:1) 
+	      out <- rbind(object$mu[subset,,i], out)
+           }	       
+
+   return(out)
 
 }
