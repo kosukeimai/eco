f0643737ab25dcde5f2b6fd3637adaed87364b82
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2005-06-28 21:40:51 +0000

    takes both eco and ecoNP parameters. for ecoNP, output is (ndraws*nobs, 2,2)

--- R/varcov.R
+++ R/varcov.R
@@ -1,7 +1,7 @@
 varcov <- function(object, ...)
   UseMethod("varcov")
 
-varcov.ecoNP <- function(object, subset = NULL, ...) {
+varcov.junk <- function(object, subset = NULL, ...) {
   if (is.null(subset))
     subset <- 1:nrow(object$Sigma[,,1])
   else if (max(subset) > nrow(object$Sigma[,,1]))
@@ -38,13 +38,24 @@ varcov.eco <- function(object, subset = NULL, ...) {
     stop(paste("invalid input for `subset.' only", nrow(object$mu), "draws are stored."))
 
   p <- ncol(object$mu)
-  n <- length(subset)
-  Sigma <- array(0, c(p, p, n))
-  cov <- object$Sigma
+  if (length(dim(object$Sigma))==2) {
+    nobs <-1
+    n <- length(subset)
+    Sigma <- array(0, c(p, p, n))
+    cov <- object$Sigma[subset,]
+  }
+  else if (length(dim(object$Sigma))==3) {
+   nobs <- dim(object$Sigma)[3] 
+   n <- length(subset)* nobs
+    Sigma <- array(0, c(p, p, n))
+    cov <- NULL
+    for (j in nobs:1) cov <- rbind(object$Sigma[subset,,j],cov)
+  }
+
   for (i in 1:n) {
     count <- 1
     for (j in 1:p) {
-      Sigma[j,j:p,i] <- cov[subset[i],count:(count+p-j)]
+      Sigma[j,j:p,i] <- cov[i,count:(count+p-j)]
       count <- count + p - j + 1
     }
     diag(Sigma[,,i]) <- diag(Sigma[,,i]/2)
