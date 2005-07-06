90b3fda2e14a191a0f7f1561d53615ea7b26c7ff
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2005-07-06 05:08:18 +0000

    separate print.summary.eco and ecoNP

--- R/print.summary.eco.R
+++ R/print.summary.eco.R
@@ -1,48 +1,26 @@
 print.summary.eco <- function(x, digits=max(3, getOption("digits")-3), ...) {
-  cat("\nCall: ") 
-  cat(paste(deparse(x$call), "\n", sep="\n", collapse="\n"))
+	cat("\nCall: ") 
+  	cat(paste(deparse(x$call), "\n", sep="\n", collapse="\n"))
 
-  if (any(class(x)=="summary.ecoNP"))
-     {
-       cat("\nAggregate Estimates:\n")
-       printCoefmat(x$agg.table, digits=digits, na.print="NA",...)
-       cat("\nNumber of Units:", x$n.obs)
-       cat("\nNumber of Monte Carlo Draws:", x$n.draws)
-
-       if (!is.null(x$param.table)) {
-         tt <- x$param.table
-         cat("\nParameter Estimates of mu1:\n")
-         printCoefmat(tt$mu1.table, digits=digits, na.print="NA",...)
-         cat("\nParameter Estimates of mu2:\n")
-         printCoefmat(tt$mu2.table, digits=digits, na.print="NA",...)
-         cat("\nParameter Estimates of Sigma11:\n")
-         printCoefmat(tt$Sigma11.table, digits=digits, na.print="NA",...)
-        cat("\nParameter Estimates of Sigma12:\n")
-         printCoefmat(tt$Sigma12.table, digits=digits, na.print="NA",...)
-        cat("\nParameter Estimates of Sigma22:\n")
-         printCoefmat(tt$Sigma22.table, digits=digits, na.print="NA",...)
-       }
-     }
-   else {
-      if (!is.null(x$param.table)) {
-        cat("\nParameter Estimates:\n")
-        printCoefmat(x$param.table, digits=digits, na.print="NA",...)
-      }
+	if (!is.null(x$param.table)) {
+           cat("\nParameter Estimates:\n")
+           printCoefmat(x$param.table, digits=digits, na.print="NA",...)
+        }
  
-      cat("\nAggregate Estimates:\n")
-      printCoefmat(x$agg.table, digits=digits, na.print="NA",...)
+        cat("\nAggregate Estimates:\n")
+        printCoefmat(x$agg.table, digits=digits, na.print="NA",...)
 
-      cat("\nNumber of Units:", x$n.obs)
-      cat("\nNumber of Monte Carlo Draws:", x$n.draws)
-   }
+        cat("\nNumber of Units:", x$n.obs)
+        cat("\nNumber of Monte Carlo Draws:", x$n.draws)
+   
   
-   if (!is.null(x$W1.table)) {
-     cat("\n\nUnit-level Estimates of W1:\n")
-     printCoefmat(x$W1.table, digits=digits, na.print="NA",...)
-     cat("\n\nUnit-level Estimates of W2:\n")
-     printCoefmat(x$W2.table, digits=digits, na.print="NA",...)
-   }
+        if (!is.null(x$W1.table)) {
+           cat("\n\nUnit-level Estimates of W1:\n")
+           printCoefmat(x$W1.table, digits=digits, na.print="NA",...)
+           cat("\n\nUnit-level Estimates of W2:\n")
+           printCoefmat(x$W2.table, digits=digits, na.print="NA",...)
+        }
 
-  cat("\n")
-  invisible(x)
+      cat("\n")
+      invisible(x)
 }
