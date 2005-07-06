f1dcbda5a52a26e33bf622a492909d5402c92e7f
kosukeimai <kimai@princeton.edu>
2005-07-06 20:37:09 +0000

    up

--- R/coef.eco.R
+++ R/coef.eco.R
@@ -1,22 +1,15 @@
-
-#coef <- function(object, ...)
-#  UseMethod("coef")
-
 coef.eco <- function(object, subset = NULL, ...) {
   if (is.null(subset))
     subset <- 1:nrow(object$mu)
   else if (max(subset) > nrow(object$mu))
     stop(paste("invalid input for `subset.' only", nrow(object$mu), "draws are stored."))
-
-    if (length(dim(object$mu))==2) out <- object$mu[subset,]
-	else if (length(dim(object$mu))==3) 
-           {
-	      out <- NULL
-	      nobs <- dim(object$mu)[3]
-	      for ( i in nobs:1) 
-	      out <- rbind(object$mu[subset,,i], out)
-           }	       
-
-   return(out)
-
+  
+  if (length(dim(object$mu))==2) out <- object$mu[subset,]
+  else if (length(dim(object$mu))==3) {
+    out <- NULL
+    nobs <- dim(object$mu)[3]
+    for ( i in nobs:1) 
+      out <- rbind(object$mu[subset,,i], out)
+  }	       
+  return(out)
 }
