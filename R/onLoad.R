cc59066504cb684761ceabfdcc36cc44a2be453a
kosukeimai <kimai@princeton.edu>
2005-07-06 14:23:02 +0000

    up

--- R/onLoad.R
+++ R/onLoad.R
@@ -2,6 +2,6 @@
   mylib <- dirname(system.file(package = pkg))
   title <- packageDescription(pkg, lib = mylib)$Title
   ver <- packageDescription(pkg, lib = mylib)$Version
-  cat(paste(pkg, ": ", title, "\nVersion", ver, "\n", sep=""))
+  cat(paste(pkg, ": ", title, "\nVersion ", ver, "\n", sep=""))
 }
 
