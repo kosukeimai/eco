0af91d476f8a6066aee02f594330fa987e3f4181
ying <ying@90758a3b-f49f-4eee-9887-1358e2851c59>
2005-07-31 18:01:52 +0000

    update

--- R/coef.eco.R
+++ R/coef.eco.R
@@ -1,31 +1,34 @@
 coef.eco <- function(object, subset = NULL, ...) {
-  if (is.null(subset))
-    subset <- 1:nrow(object$mu)
-  else if (max(subset) > nrow(object$mu))
-    stop(paste("invalid input for `subset.' only", nrow(object$mu), "draws are stored."))
 
-  if (length(dim(object$mu))==2) {
-    if (dim(object$mu)[2]==2) 
-	out <- object$mu[subset,]
-    else if (dim(object$mu)[2]==3) {
-	out <- NULL 
+  mu <- object$mu
+  Sigma <- object$Sigma
+
+  if (is.null(subset))
+    subset <- 1:nrow(mu)
+  else if (max(subset) > nrow(mu))
+    stop(paste("invalid input for `subset.' only", nrow(mu), "draws are stored."))
+
+  out <- NULL
+  if (length(dim(mu))==2) {
+    if (dim(mu)[2]==2) 
+	out <- mu[subset,]
+    else if (dim(mu)[2]==3) {
         nobs <- dim(object$X)[1]
 	for ( i in 1:nobs) {
-	  temp <- object$mu[subset,1:2]+object$Sigma[subset,c(3,5)]/object$Sigma[subset,6]*(object$X[i]-object$mu[subset,3]) 
+	  temp <- mu[subset,1:2]+Sigma[subset,c(3,5)]/Sigma[subset,6]*(object$X[i]-mu[subset,3]) 
 
 	out <- rbind(out,temp)
      }
    }
 }
- else if (length(dim(object$mu))==3) {
-    out <- NULL
-    nobs <- dim(object$mu)[3]
+ else if (length(dim(mu))==3) {
+    nobs <- dim(mu)[3]
     for ( i in 1:nobs) {
-    if (dim(object$mu)[2]==2) 
-      out <- rbind(object$mu[subset,,i], out)
-    else if (dim(object$mu)[2]==3) {
+    if (dim(mu)[2]==2) 
+      out <- rbind(mu[subset,,i], out)
+    else if (dim(mu)[2]==3) {
 
-      temp <- object$mu[subset,1:2,i]+object$Sigma[subset,c(3,5),i]/object$Sigma[subset,6,i]*(object$X[i]-object$mu[subset,3,i]) 	      
+      temp <- mu[subset,1:2,i]+Sigma[subset,c(3,5),i]/Sigma[subset,6,i]*(object$X[i]-mu[subset,3,i]) 	      
       out <- rbind(out,temp)
   }
     } 
