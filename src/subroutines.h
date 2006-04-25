05c85fa762841bf652590cb66bf63906f63e2ed6
kosukeimai <kimai@princeton.edu>
2006-04-25 01:41:23 +0000

    declaring one more function

--- src/subroutines.h
+++ src/subroutines.h
@@ -12,4 +12,4 @@ void dcholdc(double **X, int size, double **L);
 double ddet(double **X, int size, int give_log);
 double ddet2D(double **X, int size, int give_log);
 void dcholdc2D(double *X, int size, double *L);
-
+void matrixMul(double **A, double **B, int r1, int c1, int r2, int c2, double **C); 
