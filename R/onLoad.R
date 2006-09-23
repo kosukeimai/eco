1e7f0505b4930491a80909e959cdfaae66b2c257
kosukeimai <kimai@princeton.edu>
2006-09-23 18:14:28 +0000

    changed it to onAttach

--- R/onLoad.R
+++
@@ -1,7 +0,0 @@
-".onLoad" <- function(lib, pkg) {
-  mylib <- dirname(system.file(package = pkg))
-  title <- packageDescription(pkg, lib = mylib)$Title
-  ver <- packageDescription(pkg, lib = mylib)$Version
-  cat(paste(pkg, ": ", title, "\nVersion ", ver, "\n", sep=""))
-}
-
